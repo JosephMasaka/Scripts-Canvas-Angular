2788b10095dc53398efaa57e92201ad63818900e
JosephMasaka <masakajoseph14@gmail.com>
2025-02-04 15:47:48 +0300

    header component simple mods

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderInfoComponent } from '../../components/header-info/header-info.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [HeaderInfoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

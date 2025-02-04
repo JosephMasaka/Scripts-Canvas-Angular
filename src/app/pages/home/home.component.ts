0c36cf6c8db6a884cbc8f1fe042fece1cba4331f
JosephMasaka <masakajoseph14@gmail.com>
2025-02-04 15:50:51 +0300

    home component: added showcase nd services section

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HomeShowcaseComponent } from '../../components/home-showcase/home-showcase.component';
+import { HeaderInfoComponent } from '../../components/header-info/header-info.component';
+import { ServicesComponent } from '../../components/services/services.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [HomeShowcaseComponent, HeaderInfoComponent, ServicesComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}

5cefb6e8dd87103a577540a932d8369b9c04b6d7
JosephMasaka <masakajoseph14@gmail.com>
2025-02-05 16:09:25 +0300

    why choose us (Value proposition component)

---
+++ src/app/components/value-proposition/value-proposition.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-value-proposition-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './value-proposition.component.html',
+  styleUrl: './value-proposition.component.css'
+})
+export class ValuePropositionComponent {
+
+}
